815ccfdf37031348305f5c51d897e1d59cafe6d1
ivan-saji <ivansajimeleth@gmail.com>
2026-08-18 20:57:49 +0530

    First prod deployment through actions 1

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello World From Staging!");
+    return res.send("Hello World From Production!");
 })
 
 app.get('/api', function (req, res) {
